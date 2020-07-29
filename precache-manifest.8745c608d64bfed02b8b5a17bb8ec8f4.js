19dee6638cf416bdcc8223ef98c5ac01b701e808
samuel <newtumi@gmail.com>
2020-07-29 17:59:21 +0100

    Updates

---
+++ precache-manifest.8745c608d64bfed02b8b5a17bb8ec8f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf5fea2abebd56f0acb2b82d725addc",
+    "url": "/monsters-board/index.html"
+  },
+  {
+    "revision": "8efcd8852decc1030851",
+    "url": "/monsters-board/static/css/main.89a52fef.chunk.css"
+  },
+  {
+    "revision": "79a6b7c7e2bdd8c3814f",
+    "url": "/monsters-board/static/js/2.bb2c6cf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-board/static/js/2.bb2c6cf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8efcd8852decc1030851",
+    "url": "/monsters-board/static/js/main.d5814212.chunk.js"
+  },
+  {
+    "revision": "40f8c3e8068682589fb8",
+    "url": "/monsters-board/static/js/runtime-main.64696c9c.js"
+  }
+]);
